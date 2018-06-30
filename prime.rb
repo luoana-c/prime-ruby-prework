8282cfdcd97ee0b2cd1f8ceabd00a05e5ed559bf
Luoana Chirita <luoana.livadariu@gmail.com>
2018-06-30 13:14:26 +0000

    prime

--- prime.rb
+++ prime.rb
@@ -1,1 +1,24 @@
-# Add  code here!
+def range_array(number)
+  test_num = (2..(number - 1)).to_a
+  test_num
+end
+
+def remainder(number)
+  test_num = range_array(number)
+  new_array = []
+  for item in test_num
+    rem = number % item
+    new_array.push(rem)
+  end 
+  new_array
+end
+
+
+def prime?(number)
+  remainder_arr = remainder(number)
+  prime = true
+  if number < 2 || remainder_arr.include?(0) 
+    prime = false
+  end
+  prime
+end
